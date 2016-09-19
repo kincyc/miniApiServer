b7e3260010af9e8510d359f799f61d18924718ec
Kincy Clark <source@kincy.com>
2016-09-19 09:06:23 -0700

    command line options
    added command line options for reponse text and port_number

--- miniApiServer.py
+++ miniApiServer.py
@@ -8,25 +8,33 @@ import os
 import re
 import json
 import string
+from optparse import OptionParser
 
-PORT_NUMBER = 8080
+parser = OptionParser()
+parser.add_option("-p", "--port", dest="port_number", help="which port to listen", metavar="PORT_NUMBER", default=8080, type="int")
+parser.add_option("--noheaders", dest="noheaders", action="store_true", default=False, help="don't output headers in response json")
+parser.add_option("--notext", dest="notext", action="store_true", default=False, help="don't output random text in response json")
 
-# we open a text file and convert and split it into lines
-lines = open('/usr/share/doc/vmware-tools/open_source_licenses.txt').read().splitlines()
+(options, args) = parser.parse_args()
 
-words = []
-# compile regex for later
-pattern = re.compile('[\W_]+')
+PORT_NUMBER = options.port_number
 
-# we then loop through the lines and split them into words
-for l in lines:
-	# split each line into words
-	for w in l.split():
-		# remove all non-alphanumeric characters, capword them then append to array
-		words.append(re.sub(pattern, '', string.capwords(w)))
+print(options.notext, options.noheaders)
 
-# this filters out all the empty words in the array
-words = filter(None, words)
+if not options.notext:
+	# we open a text file and convert and split it into lines	
+	lines = open('/usr/share/doc/vmware-tools/open_source_licenses.txt').read().splitlines()
+	words = []
+	# compile regex for later
+	pattern = re.compile('[\W_]+')
+	# we then loop through the lines and split them into words
+	for l in lines:
+		# split each line into words
+		for w in l.split():
+			# remove all non-alphanumeric characters, capword them then append to array
+			words.append(re.sub(pattern, '', string.capwords(w)))
+	# this filters out all the empty words in the array
+	words = filter(None, words)
 
 #This class will handles any incoming request, regardless of path submitted 
 class myHandler(BaseHTTPRequestHandler):
@@ -39,17 +47,22 @@ class myHandler(BaseHTTPRequestHandler):
 		
 		# build a json response out of the words
 		myJsonResponse = dict()
-		for x in range(0,10):
-			element =random.choice(words)
-			data = ""
-			for x in range(0,random.randint(0,20)):	
-				data = random.choice(words) + " " + data
 		
-			myJsonResponse[element] = data.strip()
+		if not options.notext:
+			# if headers are turned off
+			for x in range(0,10):
+				element =random.choice(words)
+				data = ""
+				for x in range(0,random.randint(0,20)):	
+					data = random.choice(words) + " " + data
+			
+				myJsonResponse[element] = data.strip()
 
-		myJsonResponse['headers'] = {}
-		for k in self.headers:
-			myJsonResponse['headers'][k] =  self.headers[k]
+		if not options.noheaders:
+		# if random text is turned off
+			myJsonResponse['headers'] = {}
+			for k in self.headers:
+				myJsonResponse['headers'][k] =  self.headers[k]
 		
 		print(myJsonResponse)
 		
